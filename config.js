ca5df7eca1258d09953e1df418241b0164fa237a
Thai Do <thai.hl.do@gmail.com>
2020-11-16 17:28:26 -0800

    added a config file to make it more modular

---
+++ config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  siteTitle: 'Gatsby Starter Resume', // <title>
+  manifestName: 'Resume',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/resume`, // This path is subpath of your hosting https://domain/portfolio
+  firstName: 'Thai',
+  lastName: 'Do',
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/DespoTron',
+    },
+    {
+      icon: 'fa-linkedin-in',
+      name: 'Linkedin',
+      url: 'www.linkedin.com/in/hdthaido',
+    },
+  ],
+  email: 'hdthaido@gmail.com',
+  phone: '778-997-0696',
+  address: 'Richmond, British Columbia',
+};
