2533d50857098e018f05c4ff04d34a6758f3f145
Thai Do <thai.hl.do@gmail.com>
2021-01-28 18:23:28 -0800

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-5cacfc424cf804cf49da.js"
+    "url": "webpack-runtime-e805dc92f079aa83d993.js"
   },
   {
     "url": "framework-83e11af5cf111be5ad89.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "11424a23fae56558c435cb926c45d17e"
+    "revision": "38e9080c8e5270de8640e5696cb49eef"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "9cdf61d4adb8ffe9b7157136c278c702"
+    "revision": "5a2e7ee53fa5f130755a2931bb7f8c11"
   },
   {
     "url": "polyfill-c4b2a7b6e678cd28550e.js"
